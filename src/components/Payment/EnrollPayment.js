1579779740a35f923f7c6c43070388c07c6db959
Preetham-jayam <preethamjayam2004@gmail.com>
2024-04-17 14:24:15 +0530

    Paypal integrated

--- src/components/Payment/EnrollPayment.js
+++ src/components/Payment/EnrollPayment.js
@@ -1,121 +1,94 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import "./EnrollPayment.css";
 import { useSelector } from "react-redux";
 import { useGetUserDetailsQuery } from "../../Slices/usersApiSlice";
+import { useGetCourseDetailsQuery } from "../../Slices/courseApiSlice";
 import { useEnrollCourseMutation } from "../../Slices/studentApiSlice";
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import Loader from "../Loader/Loader";
+import { PayPalScriptProvider, PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import './EnrollPayment.css';
 
 const EnrollPayment = () => {
-  const auth = useSelector((state) => state.auth);
-  const [user, setUser] = useState(null);
-
   const { cid } = useParams();
-  const { data, isLoading: userLoading } = useGetUserDetailsQuery(auth.userInfo?.userId);
-  const [enrollCourse]=useEnrollCourseMutation();
-
+  const auth = useSelector((state) => state.auth);
+  const { data: userData, isLoading: userLoading } = useGetUserDetailsQuery(auth.userInfo?.userId);
+  const { data: courseData, isLoading: courseLoading } = useGetCourseDetailsQuery(cid);
+  const [enrollCourse] = useEnrollCourseMutation();
   const navigate = useNavigate();
+  const [showSpinner, setShowSpinner] = useState(false);
 
-  useEffect(() => {
-    if (data) {
-      setUser(data.user);
-    }
-   
-  }, [data]);
-
-  const [cardNumber, setCardNumber] = useState("");
-  const [expiryDate, setExpiryDate] = useState("");
-  const [cvv, setCvv] = useState("");
-  const [errors, setErrors] = useState({});
-
-  const validateForm = () => {
-    const errors = {};
-
-    if (!/^\d{16}$/.test(cardNumber)) {
-      errors.cardNumber = "Invalid card number";
-    }
-
-    if (!/^\d{2}\/\d{4}$/.test(expiryDate)) {
-      errors.expiryDate = "Invalid expiry date";
-    }
-
-    if (!/^\d{3}$/.test(cvv)) {
-      errors.cvv = "Invalid CVV";
-    }
-
-    return errors;
-  };
-
-  const handlePayment = async () => {
-    const formErrors = validateForm();
-   
-    if (Object.keys(formErrors).length === 0) {
-      try {
-        await enrollCourse({ userId: user.id, courseId: cid });
-
-        toast.success("Course enrolled successfully", { autoClose: 3000 });
-        
-        navigate(`/courseContent/${cid}`);
-      
-      } catch (error) {
-        console.error(error.message);
-        toast.error("Error enrolling in the course", { autoClose: 3000 });
-      }
-    } else {
-      setErrors(formErrors);
+  const handleSuccess = async (details, data) => {
+    try {
+      await enrollCourse({ userId: userData.user.id, courseId: cid });
+      toast.success("Course enrolled successfully", { autoClose: 3000 });
+      navigate(`/courseContent/${cid}`);
+    } catch (error) {
+      console.error(error.message);
+      toast.error("Error enrolling in the course", { autoClose: 3000 });
     }
   };
 
-  if (userLoading ) {
+  const handleError = (err) => {
+    console.error("An error occurred during PayPal checkout:", err);
+    toast.error("Error during payment. Please try again.", { autoClose: 3000 });
+  };
+
+  if (userLoading || courseLoading || !userData || !courseData) {
     return <Loader />;
   }
 
+  const createOrder = (data, actions) => {
+    setShowSpinner(true);
+    return actions.order.create({
+      intent: "CAPTURE",
+      purchase_units: [
+        {
+          description: courseData.course.title,
+          amount: {
+            currency_code: "USD",
+            value: courseData.course.price,
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data) => {
+    setShowSpinner(false);
+    handleSuccess(null, data);
+  };
+
   return (
     <div className="enroll-payment-container">
-      <h2>Enrollment Payment Page</h2>
-      <form className="enroll-payment-form">
-        <label>
-          Card Number:
-          <input
-            type="text"
-            value={cardNumber}
-            onChange={(e) => setCardNumber(e.target.value)}
-            placeholder="Enter your card number"
-            className="enroll-input"
-          />
-          {errors.cardNumber && <span className="enroll-error">{errors.cardNumber}</span>}
-        </label>
-        <br />
-        <label>
-          Expiry Date:
-          <input
-            type="text"
-            value={expiryDate}
-            onChange={(e) => setExpiryDate(e.target.value)}
-            placeholder="MM/YYYY"
-            className="enroll-input"
-          />
-          {errors.expiryDate && <span className="enroll-error">{errors.expiryDate}</span>}
-        </label>
-        <br />
-        <label>
-          CVV:
-          <input
-            type="text"
-            value={cvv}
-            onChange={(e) => setCvv(e.target.value)}
-            placeholder="Enter CVV"
-            className="enroll-input"
-          />
-          {errors.cvv && <span className="enroll-error">{errors.cvv}</span>}
-        </label>
-        <br />
-        <button type="button" onClick={handlePayment} className="enroll-button">
-          Proceed to Payment
-        </button>
-      </form>
+    <h2>Enroll Course</h2>
+    <div className="enroll-payment-form">
+        <h3>Course: <span style={{ color: '#06bbcc' }}>{courseData.course.title}</span></h3>
+        <h3>Instructor Name: <span style={{ color: '#06bbcc' }}>{courseData.course.instructorName}</span></h3>
+        <h3>Price: <span style={{ color: 'red' }}>{courseData.course.price}</span></h3>
+      <PayPalScriptProvider options={{ clientId: "AX-vMceEk8bWnS2CYWuTkilAtyx4TSiD6-VinlotsfKd0mIiTY_X1Ydy3sqBdVO0U9qvxfeOVlC2B_tm" }}>
+        <ButtonWrapper createOrder={createOrder} onApprove={onApprove} showSpinner={showSpinner} onError={handleError} />
+      </PayPalScriptProvider>
     </div>
+  </div>
+  
+  );
+};
+
+const ButtonWrapper = ({ createOrder, onApprove, showSpinner, onError }) => {
+  const [{ isPending }] = usePayPalScriptReducer();
+  console.log(isPending);
+  return (
+    <>
+      {showSpinner && isPending && <div className="spinner" />}
+      <PayPalButtons
+        style={{ layout: "vertical" }}
+        createOrder={createOrder}
+        onApprove={(data) => onApprove(data)}
+        onError={(err) => onError(err)}
+        forceReRender={[showSpinner]}
+      />
+    </>
   );
 };
 
